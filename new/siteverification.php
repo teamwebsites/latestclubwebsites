4a467901402f3f60e8e295a19a9df7950feebf4d
teamwebsites <morgan@teamwebsites.co.uk>
2018-02-14 19:59:53 +0000

    Setting Things Up

---
+++ new/siteverification.php
@@ -0,0 +1,19 @@
+<?php if(true === get_theme_mod('site-isverified')){ 
+
+echo '
+
+<div id="verifiedclub">
+
+<i class="fas fa-check-circle"></i>
+
+   
+
+<span>This club has been verified as being affiliated to their parent County FA</span>
+    
+</div>
+
+';
+
+}
+
+?>
---
+++ new/templates/header.php
@@ -0,0 +1,606 @@
+<div class="poweredby">
+<div class="container">
+    
+<div class="pbfloat left">
+<a href="https://www.teamwebsites.co.uk/" target="_blank" title="Football Club Website powered by Team Websites - teamwebsites.co.uk">
+<img src="https://www.teamwebsites.co.uk/wp-content/uploads/2017/08/blue-logo.png" height="33">    
+<div class="twcredit headercredit">Powered by Team Websites</div>    
+</a>
+</div>
+
+<div class="pbfloat right">
+<div class="twcredit headercredit">In association with <span>Grassroots Football</span></div>    
+<img src="https://www.teamwebsites.co.uk/wp-content/uploads/2018/02/grassrootsfootball.png" height="43">    
+</div>  
+</div>  
+</div>
+
+<?php custom_breadcrumbs(); ?>
+
+<style>
+.srch .search-submit, .srch .search-field {border: none !important; font-size: 10.5px; padding: 4px 7px !important; border-radius: 0px !important;}
+.swiper-slide {background: rgba(154, 154, 154, 0.68); margin-bottom: 10px;}
+.getsmllr .icon {margin: 0px !important; padding-right: 8px !important;}
+.pastmatch {display: flex !important; display: -webkit-flex !important; -webkit-align-items: center !important; align-items: center !important; justify-content: center; -webkit-justify-content: center !important;}
+#headerquicklinks ul, .flickity-slider, .flickity-enabled.is-draggable .flickity-viewport, #teams-title-sec {display: flex; display: -webkit-flex; -webkit-align-items: center; align-items: center;}
+.carousel {width: 100% !important;}
+
+.flickity-enabled.is-draggable {background: lightgrey;}
+#bloginfosec {width: calc(100% - 72px) !important;}
+.pbfloat a {text-decoration: none; color: inherit !important;}
+.header .container {z-index: 11; position: relative;}
+#desktop-menu a {color: #fff !important;}
+#desktop-menu .sub-menu a {color: #000 !important;}
+.header a {color: inherit !important; text-decoration: none !important;}
+.twcredit {font-weight: bold;}
+header.sticky, .header {opacity: 0; animation: fadeIn 1s ease-in both;}
+.header {animation-delay: 0.1s;}
+
+.flickity-enabled.is-draggable .flickity-viewport {height: 300px;}
+
+header.sticky {animation-delay: 1.5s;}
+@keyframes fadeIn {
+	from {
+		opacity: 0;
+		transform: translate3d(0, -20%, 0);
+	}
+	to {
+		opacity: 1;
+		transform: translate3d(0, 0, 0);
+	}
+}
+.left div {text-align: left !important;}
+.left img {display: block; float: left; margin-right: 6.5px !important;}
+.right img {display: block; float: left; margin-left: 6.5px !important;}
+.poweredby .left {justify-content: flex-start !important; text-align: right; align-items: center; display: flex; width: 70%; float: left;}
+.poweredby .right {justify-content: flex-end !important; text-align: right; align-items: center; width: 30%; display: flex;}
+.poweredby {padding: 6px 0px; color: #757575; font-size: 10.5px; font-family: inherit; background: #e2e2e2; display: block; clear: both; width: 100%;}
+.poweredby .container, .poweredby a {display: flex; display: -webkit-flex; -webkit-align-items: center; align-items: center;}
+.poweredby:after {content: ""; display: block; clear: both; height: 0px;}
+.poweredby .headercredit {float: left; margin-right: 4px !important; font-family: 'Montserrat', sans-serif !important;}
+.poweredby .headercredit, .poweredby img {margin: 0px;}
+body {background: #ececec !important;}
+#bannerright {overflow: hidden; height: 100%; position: absolute; right: 0px; display: block; top: 0px; bottom: 0px; left: 0px; }
+header.sticky .container {max-width: 100% !important; padding: 1px 0px !important;}
+.header-right {overflow: hidden; position: relative; padding-right: 10px !important;}
+.header {position: relative;}
+#bannerright:before {content: ""; display: block; width: 0; height: 0; border-style: solid; border-width: 0 0 200px 55em; border-color: transparent transparent rgba(165,165,165,0.22) transparent; position: absolute; right: 0px; bottom: 0px;}
+header.sticky .header-right #affiliationheaderpic {margin-left: 5.5px !important;}
+.header-right #affiliationheaderpic {margin-left: 11.5px;}
+header.sticky .social-icon#twitter:before {margin-left: 0px !important;}
+header.sticky .social-icon {width: 20px !important; height: 20px !important; font-size: 11px !important;}
+.header .social-icon {margin-right: 4px; font-family: FontAwesome; border-radius: 50%; width: 30px; height: 30px; position: relative;}
+.social-icon#facebook {background: #3B5998; color: #fff !important;}
+.social-icon:before {width: 100%; height: 100%; -webkit-justify-content: center; justify-content: center; display: flex; display: -webkit-flex; -webkit-align-items: center; align-items: center; position: absolute; text-align: center;}
+.social-icon#twitter {background: lightblue; color: #fff !important;}
+.social-icon#facebook:before {content: "\f09a"; margin-top: 0.5px;}
+.social-icon#twitter:before {content: "\f099"; margin-left: 0.75px; margin-top: 0.5px;}
+.social-icon#twitter {background: #00aced; color: #fff;}
+header.sticky .header-right img {height: 25px !important;}
+.header .header-left {white-space: nowrap; text-overflow: ellipsis; overflow: hidden;}
+.header .header-left, .header .header-right {height: 55px;}
+.header .container {display: flex; align-items: center; flex-wrap: wrap;}
+.header .header-left {width: 65%; float: left;} .header .header-right {width: calc(35% - 10px); float: right; text-align: right;}
+.header .header-right, .header .container {display: flex; display: -webkit-flex; -webkit-align-items: center; align-items: center;}
+.header .header-right {-webkit-justify-content: flex-end; justify-content: flex-end;}
+.header .nav {margin-bottom: 0px !important;}
+header.sticky .newmenu .container {margin: 0px auto !important;}
+.header .newmenu {padding-bottom: 0px !important;}
+.newmenu .container {margin: 7px auto !important;}
+header nav .container .nav {margin-bottom: 0px !important;}
+header.sticky #desktop-menu ul {margin-bottom: 0px !important;}
+.social-icon {color: #fff !important;}
+.header nav .container .nav:after { content: ""; display: block; clear: both; height: 0px; }
+header.sticky nav {clear: both !important; display: block !important;}
+header.sticky .fullwidthnav {padding: 10px 0px !important;}
+header.sticky #logo {height: 33px !important; margin-right: 10px !important;} header.sticky h1 {font-size: 14px !important;} header.sticky h2 {display: none !important;}
+#desktop-menu .nav {margin-top: 0px !important;}
+div#desktop-menu:after, .menu-header-navigation-container:after { display: block; clear: both; height: 0px; content: ""; }
+@media screen and (min-width: 951px) { .swiper-slide {width: calc(25% - 10px) !important;} header.sticky .mobilemenu-1 {display: none !important;} }
+.newmenu .container {font-size: 13.5px !important; padding: 3px 0px !important;}
+.header nav .container .nav {padding: 0px !important;}
+.header nav {display: block; clear: both; margin-top: 0px !important; width: 100% !important;}
+.header {background: #fdfdfd; display: flex; align-items: center; justify-content: center; flex-wrap: wrap;}
+.header .header-left, .header-right {  padding: 0px 0px; flex-direction: row;}
+header.sticky .header-left, header.sticky .header-right {padding: 3px 0px  !important;}
+.header {clear: both;}
+.header h1 {overflow: visible !important;}
+.header h1, .header h2 {display: block; text-overflow: ellipsis; white-space: nowrap; overflow: hidden;} .headerblock {text-overflow: ellipsis; white-space: nowrap; overflow: hidden; width: calc(100% - 63px);}
+.headerblock {display: block;}
+.header .header-left {display: flex; align-items: center; justify-content: flex-start; flex-wrap: wrap; flex-direction: row;}
+.header h1 {margin-bottom: 5px; margin-top: 0px; font-weight: 600; font-size: 19px;}
+.header #logo {margin-right: 17px !important;}
+.header h2 {font-weight: 500; font-size: 13px; margin-bottom: 0px; margin-top: 5px;}
+#headerquicklinks:after {content: ""; clear: both; display: block; height: 0px;}
+#headerquicklinks {padding: 7.5px 0px; width: 100%; text-align: right; font-size: 12px; background: #eee;}
+#headerquicklinks ul {float: right; list-style: none; margin: 0px; padding: 0px;}
+#headerquicklinks ul li {padding-left: 6.5px; text-transform: uppercase; text-align: center; float: left; display: block;}
+#headerquicklinks ul li:after {margin-left: 7px; content: "/"; display: block; float: right;}
+#headerquicklinks ul li:last-child:after {display: none !important;}
+#adjustslightly {overflow: hidden;}
+
+.imgholder2, .the-individual-article h3 {background: #eee;} .imgholder2 {overflow: hidden; height: 195px;} .the-individual-article img {visibility: hidden;}
+.the-individual-article .featuredimage:hover, .the-individual-article .featuredimage:focus {border-bottom-left-radius: 0px !important;}
+.the-individual-article .featuredimage {border-bottom-left-radius: 15px; width: 100%; height: 100%;}  .the-individual-article h3 {padding-top: 9px !important; padding-bottom: 9px !important; background: #eee; padding-left: 11px; padding-right: 10px; font-family: arial !important; margin-top: 0px !important;} 
+@media screen and (max-width: 750px) { .left {width: 83.73px !important;} .right {width: calc(100% - 90.23px) !important;} .left .headercredit {display: none !important;} .headercredit {font-size: 70% !important;} #bannerright:before{border-width: 0 0 200px 190px !important;} .header-right .social-icon {display: none !important;} }
+@media screen and (max-width: 950px) and (min-width: 751px) { #bannerright:before{border-width: 0 0 200px 360px !important;} }
+
+@media screen and (max-width: 480px) { .swiper-wrapper {margin-left: calc(0px + 5px) !important;} .swiper-slide {height: 250px; width: calc(100% - 10px) !important;} }
+@media screen and (min-width: 481px) and (max-width: 950px) {  .swiper-wrapper {margin-left: calc(-150% + 5px);} .swiper-slide {height: 250px; width: calc(50% - 10px) !important;} }
+
+@media screen and (max-width: 950px){ .header #logo, #affiliationheaderpic {max-height: 30px !important;} .header h1 {font-size: 65% !important;} .header h2 {font-size: 40% !important;} #st-2, .mobile-footer {z-index: 111111111111111111111111111111111111111111 !important;} .newmenu .container {padding: 0px !important; margin: 0px !important;} #adjustslightly {width: 100%;} .the-individual-article {width: 100% !important; clear: both; padding-right: 5px; padding-left: 5px;}}
+@media screen and (min-width: 951px) { .swiper-slide {height: 275px;} .the-individual-article {position: relative;}.the-individual-article{width: calc(50% - 9px); padding-right: 8px; float: left !important;}}
+.the-individual-article {font-family: arial !important; margin-bottom: 23px; float: left;}
+#blogpage {margin-top: 15px;}
+#blogpage a {position: relative;}
+#blogpage a:nth-child(4n):after { content: '\a'; white-space: pre; display: block; clear: both; width: 100%; height: 0px; }
+</style>
+
+<div id="headerquicklinks">
+<div class="container">
+<ul>
+<li>Online Shop</li>
+<li>Facebook</li>
+<li>Twitter</li>
+<li>Instagram</li>
+<li>
+    <script>jQuery(document).ready(function($) {
+
+ $(".search-field").focus(function(){
+   $(this).parent().removeClass("round");
+   $(this).parent().addClass("bluebg");
+
+  }).blur(function(){
+       $(this).parent().removeClass("bluebg");
+   $(this).parent().addClass("round");
+  })
+
+}); </script>
+<form role="search" method="get" class="srch" action="<?php echo site_url('/'); ?>">
+    <label>
+        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+        <input type="search" class="search-field"
+            placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+            value="<?php echo get_search_query() ?>" name="s"
+            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+    
+    <input type="submit" class="search-submit"
+        value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+    </label>    
+</form>    
+    
+    <span style="display: none;">Search <i class="fa fa-search"></i></span></li>
+</ul>       
+</div> 
+</div>
+
+<header class="banner header" role="banner" style="color: #000 !important;">
+
+<div class="container outerheadercontainer" style="width: 100%;">
+
+<div id="adjustslightly" style="overflow: hidden;">
+
+<div id="bannerright"></div>      
+    
+<div class="container" style="padding: 13px 0px;">
+
+<div class="header-left">
+    
+<a href="<?php echo site_url(); ?>" rel="bookmark" title="<?php bloginfo('name'); ?> - Official Club Website">    
+
+<?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+if ( has_custom_logo() ) {
+echo '<img id="logo" src="'. esc_url( $logo[0] ) .'">';
+} else {
+echo '';
+} ?>
+
+</a>
+
+<a id="bloginfosec" rel="bookmark" href="<?php echo site_url(); ?>" title="<?php bloginfo('name'); ?> - Official Club Website">
+<div class="headerblock">
+<h1><span><?php bloginfo('name'); ?></span> <?php get_template_part( 'siteverification' ); ?></h1>
+<h2><?php echo get_theme_mod( 'club_nickname' ); ?></h2>
+</div>
+</a>
+
+</div>
+
+<div class="header-right">
+
+<a href="https://www.facebook.com/teamwebsites/" class="social-icon" id="facebook"></a>
+<a href="https://www.twitter.com/myteamwebsite/" class="social-icon" id="twitter"></a>
+<img src="https://teamwebsites.co.uk/images/fa%20charter%20standard%20club.gif" id="affiliationheaderpic" height="45" alt="Nothing to see here!">
+
+</div>
+
+</div>        
+    
+</div>
+
+
+
+  <nav id="site-navigation" class="newmenu nav-primary fullwidthnav main-navigation" role="navigation">
+
+      <div class="container">
+
+          
+
+  <nav id="site-navigation" class="main-navigation mobilemenu-1" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
+
+   <div class="dropdown3 mobile-menu">
+
+       
+
+     <button onclick="myFunction('Demo1')" class="w3-btn w3-block w3-black w3-left-align">Menu <i class="fa fa-bars" aria-hidden="true"></i></button>
+
+<div id="Demo1" class="w3-container w3-hide">
+
+  <?php
+
+									wp_nav_menu( array(
+
+										'theme_location' => 'primary',
+
+										'menu_class'     => 'primary-menu',
+
+									 ) );
+
+								?>
+
+</div>
+
+       
+
+  </div>
+
+</nav><!-- .main-navigation -->            
+
+             
+
+             
+
+             <script>
+
+/* When the user clicks on the button, 
+
+toggle between hiding and showing the dropdown content */
+
+function myFunction3() {
+
+    document.getElementById("myDropdown3").classList.toggle("with4");
+
+}
+
+
+
+// Close the dropdown if the user clicks outside of it
+
+window.onclick = function(event) {
+
+  if (!event.target.matches('.dropbtn3')) {
+
+
+
+    var dropdowns = document.getElementsByClassName("dropdown-content3");
+
+    var i;
+
+    for (i = 0; i < dropdowns.length; i++) {
+
+      var openDropdown = dropdowns[i];
+
+      if (openDropdown.classList.contains('with4')) {
+
+        openDropdown.classList.remove('with4');
+
+      }
+
+    }
+
+  }
+
+}
+
+</script>
+
+          
+
+      <div id="desktop-menu">
+
+          
+
+        <?php
+
+      if (has_nav_menu('primary')) :
+
+        wp_nav_menu(['theme_location' => 'primary', 'menu_class' => 'nav']);
+
+      endif;
+
+      ?>  
+
+          
+
+      </div>
+
+  
+
+      </div>
+
+    </nav>    
+    
+</div>
+
+
+</header>
+
+
+
+
+
+<header class="banner top-header" role="banner" style="display: none !important;">
+    
+<div style="height: 100%; width: 100%;" class="top-header-st"></div>    
+
+
+  <div class="container">
+
+      
+
+    <div style="width: 100%; clear: both;"> 
+
+      
+
+    <div class="col-lg-8">
+
+        
+
+    <div class="logo-header">
+
+    
+
+    <a href="<?php echo site_url(); ?>" title="<?php bloginfo('name'); ?> - Official Club Website">
+
+        <?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+
+$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+
+if ( has_custom_logo() ) {
+
+        echo '<img id="logo" src="'. esc_url( $logo[0] ) .'">';
+
+} else {
+
+        echo '';
+
+} ?>
+
+        </a>
+
+    
+
+    </div>   
+
+    
+
+    <div class="site-identity">
+
+        
+
+    <h1><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a> <?php get_template_part( 'siteverification' ); ?></h1>    
+
+    
+
+    <?php if ( get_theme_mod( 'club_nickname' ) ) : ?>
+
+        
+
+    <h2><?php echo get_theme_mod( 'club_nickname' ); ?></h2>
+
+    
+
+    <?php else : ?>
+
+
+
+    <?php endif; ?>
+
+        
+
+    </div>
+
+        
+
+    </div>
+
+    
+
+    <div class="col-lg-4 header-social" style="position: relative;">
+
+        
+
+    <div style="display: block; float: right; margin-left: auto; position: relative; height: 100%;">  
+
+    
+
+    <?php if ( get_theme_mod( 'fb_pg_url' ) ) : ?>
+
+        
+
+    <a href="<?php echo esc_url( get_theme_mod( 'fb_pg_url' ) ); ?>" title="Like us on Facebook" target="_blank"> <button class="facebook">Facebook</button> </a>
+
+    
+
+    <?php else : ?>
+
+
+
+    <?php endif; ?>
+
+    
+
+    <?php if ( get_theme_mod( 'twitter_user_name' ) ) : ?>
+
+    
+
+    <a href="https://twitter.com/<?php echo get_theme_mod( 'twitter_user_name' ); ?>" title="Follow us on Twitter" target="_blank"> <button class="twitter">Twitter</button> </a>
+
+    
+
+    <?php else : ?>
+
+
+
+    <?php endif; ?>
+
+        
+
+     </div> 
+
+        
+
+    </div>
+
+    
+
+    </div>
+
+    
+
+    <div class="removeonsmallscreens" style="clear: both; display: block;     margin-bottom: -20px; height: 0px;">
+
+    
+
+  </div>
+
+  
+
+  </div>
+
+  
+
+  
+
+  <nav id="site-navigation" class="nav-primary fullwidthnav main-navigation" role="navigation">
+
+      <div class="container">
+
+          
+
+  <nav id="site-navigation" class="main-navigation mobilemenu-1" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
+
+   <div class="dropdown3 mobile-menu">
+
+       
+
+     <button onclick="myFunction('Demo1')" class="w3-btn w3-block w3-black w3-left-align">Menu <i class="fa fa-bars" aria-hidden="true"></i></button>
+
+<div id="Demo1" class="w3-container w3-hide">
+
+  <?php
+
+									wp_nav_menu( array(
+
+										'theme_location' => 'primary',
+
+										'menu_class'     => 'primary-menu',
+
+									 ) );
+
+								?>
+
+</div>
+
+       
+
+  </div>
+
+</nav><!-- .main-navigation -->            
+
+             
+
+             
+
+             <script>
+
+/* When the user clicks on the button, 
+
+toggle between hiding and showing the dropdown content */
+
+function myFunction3() {
+
+    document.getElementById("myDropdown3").classList.toggle("with4");
+
+}
+
+
+
+// Close the dropdown if the user clicks outside of it
+
+window.onclick = function(event) {
+
+  if (!event.target.matches('.dropbtn3')) {
+
+
+
+    var dropdowns = document.getElementsByClassName("dropdown-content3");
+
+    var i;
+
+    for (i = 0; i < dropdowns.length; i++) {
+
+      var openDropdown = dropdowns[i];
+
+      if (openDropdown.classList.contains('with4')) {
+
+        openDropdown.classList.remove('with4');
+
+      }
+
+    }
+
+  }
+
+}
+
+</script>
+
+          
+
+      <div id="desktop-menu">
+
+          
+
+        <?php
+
+      if (has_nav_menu('primary')) :
+
+        wp_nav_menu(['theme_location' => 'primary', 'menu_class' => 'nav']);
+
+      endif;
+
+      ?>  
+
+          
+
+      </div>
+
+  
+
+      </div>
+
+    </nav>
+
+  
+
+</header>
+
+
+
+<div>
+  
+    
+</div>
+
